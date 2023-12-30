41123d59becf945378ee917f4bc98c319976be25
nkastratovic <nkastratovic@gmail.com>
2023-12-30 11:04:39 +0100

    Create Coupon and CouponDTO Classes.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Models
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
