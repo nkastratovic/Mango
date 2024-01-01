363077b9066005aa82899cd1f83b2f7c65f4e2d6
nkastratovic <nkastratovic@gmail.com>
2024-01-01 14:28:30 +0100

    Add Controller and Get all and Get Coupon by id.

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,48 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        public readonly AppDbContext _db;
+
+        public CouponController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.FirstOrDefault(u => u.CouponId == id);
+                return coupon;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+    }
+}
