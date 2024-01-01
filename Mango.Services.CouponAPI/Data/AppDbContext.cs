9a4a8b82d4dd80b271cc80f0b8327babc0b3da7c
nkastratovic <nkastratovic@gmail.com>
2024-01-01 11:41:42 +0100

    Create AppDbContext.

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mango.Services.CouponAPI.Models;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
