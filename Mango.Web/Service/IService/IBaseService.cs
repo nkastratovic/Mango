3d7e682804e4f7e2b255c73803ae9aa7d55070d5
nkastratovic <nkastratovic@gmail.com>
2024-01-02 21:50:56 +0100

    Add Base Service Interface.

---
+++ Mango.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.DTO;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
