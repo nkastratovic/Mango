c7472538bc7cf752556da799ecb37df1da395fa8
nkastratovic <nkastratovic@gmail.com>
2024-01-04 13:22:40 +0100

    Configure JWT options.

---
+++ Mango.Services.AuthAPI/Models/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.AuthAPI.Models
+{
+    public class JwtOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Secret { get; set; }
+    }
+}
