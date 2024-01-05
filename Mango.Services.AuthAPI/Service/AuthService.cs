afba8175b3ac95461576756c20f6d5c80502bfea
nkastratovic <nkastratovic@gmail.com>
2024-01-05 09:58:21 +0100

    Login in Action.

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -20,9 +20,35 @@ namespace Mango.Services.AuthAPI.Service
             _roleManager = roleManager;
         }
 
-        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
-            throw new NotImplementedException();
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+
+            bool isValied = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+            if (user == null || !isValied)
+            {
+                return new LoginResponseDto() { User = null, Token = "" };
+            }
+
+            //If user is found generate JWT token
+
+            UserDto userDto = new()
+            {
+                Email = user.Email,
+                ID = user.Id,
+                Name = user.Name,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDto,
+                Token = ""
+            };
+
+            return loginResponseDto;
+
         }
 
         public async Task<string> Register(RegistrationRequestDTO registrationRequestDTO)
