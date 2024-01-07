9c6532ea9750edada2a5c83c74285c1db1d58871
nkastratovic <nkastratovic@gmail.com>
2024-01-07 22:57:48 +0100

    Consuming Product API.

--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -6,6 +6,7 @@ namespace Mango.Web.Utility
     {
         public static string CouponAPIBase {  get; set; }
         public static string AuthAPIBase { get; set; }
+        public static string ProductAPIBase { get; set; }
         public const string RoleAdmin = "ADMIN";
         public const string RoleCustomer = "CUSTOMER";
         public const string TokenCookie = "JWTToken";
