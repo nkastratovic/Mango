d2dc56e32ec58a1fe2d0f7161d67586efd068fab
nkastratovic <nkastratovic@gmail.com>
2024-01-16 10:19:58 +0100

    Create Order Service.

--- Mango.Services.OrderAPI/Data/AppDbContext.cs
+++ Mango.Services.OrderAPI/Data/AppDbContext.cs
@@ -5,11 +5,12 @@ namespace Mango.Services.OrderAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
-        { 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
         }
 
         public DbSet<OrderHeader> OrderHeaders { get; set; }
         public DbSet<OrderDetails> OrderDetails { get; set; }
+     
     }
 }
