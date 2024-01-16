8eacdd1ce5f0c46b34faae67a0a883879613b8c8
nkastratovic <nkastratovic@gmail.com>
2024-01-16 09:24:43 +0100

    Mapping Config for Order API.

---
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
+
+namespace Mango.Services.OrderAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+            });
+            return mappingConfig;
+        }
+    }
+}
